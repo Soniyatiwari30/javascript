1eb2c80f148196323e8c70eab34c3fc2e40e6e45
Soniyatiwari30 <128225944+Soniyatiwari30@users.noreply.github.com>
2024-09-24 11:23:33 +0000

    datatype and variables

---
+++ .devcontainer/01-basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "soniya7777";
+var accountPassword = "12345";
+accountCity = "jaipur"
+
+// accountId =2
+accountEmail = "abc.com"
+accountPassword = "12122121"
+accountCity = "begaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
---
+++ .devcontainer/01-basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) // we are using node.js not browser
+console.log(3
++
+3
+); //code readability should be high
+
+console.log("hitesh");
+
+//PRIMITIVE DATATYPES//
+//nigint-> for ver large numbers
+//undefined-> value was not defined yet
+//null -> empty or standalone value
+// symbol-> used in raect for finding uniqueness
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
