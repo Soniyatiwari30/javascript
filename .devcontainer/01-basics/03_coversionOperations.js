289e76951eb5a6f504770f1e2ec6336acc765622
Soniyatiwari30 <128225944+Soniyatiwari30@users.noreply.github.com>
2024-09-24 11:51:03 +0000

    conversion of datatypes

---
+++ .devcontainer/01-basics/03_coversionOperations.js
@@ -0,0 +1,22 @@
+// let score = "33"
+// let a = "33abc"
+// console.log(typeof score);
+
+// let valueInNumber= Number(score);
+// console.log(typeof valueInNumber);
+
+// let newA = Number(a); 
+// console.log(typeof newA);
+// console.log(newA);
+
+let isLOggedIN = 1
+let booeanLog = Boolean(isLOggedIN)
+console.log(booeanLog);
+
+//1-> true ; 0-> false
+//"" -> false
+//"soni"->true
+
+let someNum = 30
+let stringNum = String(someNum)
+console.log(typeof stringNum);
