faa6d4c096e904c511fdd226b64daed9ae83d082
Soniyatiwari30 <128225944+Soniyatiwari30@users.noreply.github.com>
2024-09-26 11:27:40 +0000

    stack and heap

--- .devcontainer/01-basics/datatypes-summary.js
+++ .devcontainer/01-basics/datatypes-summary.js
@@ -2,16 +2,16 @@
 // 7 types:-
 /*string, Number, boolean, Null, undefined, Symbol, BigInt*/
 //js is dynamically typed language
-const score = 100
-const scoreValue =100.3
+// const score = 100
+// const scoreValue =100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123');
-const anotherId = Symbol('123')
-console.log(id === anotherId);
+// const id = Symbol('123');
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
 
 // const bigNumber = 345645765762342356n
 
@@ -19,20 +19,44 @@ console.log(id === anotherId);
 //refrence type or non-primitive data type:
 /* array, objects, functions*/
 
-const heros = ["shaktiman","naagraj","doga"]
-let myObj = {
-    name:"hitesh",
-    age: 22,
-}
+// const heros = ["shaktiman","naagraj","doga"]
+// let myObj = {
+//     name:"hitesh",
+//     age: 22,
+// }
 
-const myFunc = function(){
-    console.log("hello world");
-}
+// const myFunc = function(){
+//     console.log("hello world");
+// }
 // myFunc()
 
-console.log(typeof bigNumber);
-console.log(typeof outsideTemp);
-console.log(typeof myFunc);
-console.log(typeof heros);
-console.log(typeof myObj);
-console.log(typeof anotherId);
+// console.log(typeof bigNumber);
+// console.log(typeof outsideTemp);
+// console.log(typeof myFunc);
+// console.log(typeof heros);
+// console.log(typeof myObj);
+// console.log(typeof anotherId);
+
+
+
+//memory
+//stack(primitive) , heap(non-primitive)
+let mytoutb = "soni.com" //stack
+let anotherName = mytoutb
+anotherName = "chaiorcode"
+
+console.log(mytoutb);
+console.log(anotherName);
+
+
+
+
+
+let userOne = {
+    email:"user@google.com", //heap
+    upi:"user@ybl"
+}
+let userTwo = userOne
+userTwo.email="soni@gamil.com"
+console.log(userOne.email);
+console.log(userTwo.email);
