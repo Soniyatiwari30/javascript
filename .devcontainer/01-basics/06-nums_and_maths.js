90f4f33b6fac4b0dfe5eb565d501defc3b1cb94b
Soniyatiwari30 <128225944+Soniyatiwari30@users.noreply.github.com>
2024-09-27 09:17:18 +0000

    math.num

---
+++ .devcontainer/01-basics/06-nums_and_maths.js
@@ -0,0 +1,46 @@
+// const score = 400
+// console.log(score);
+
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)//it converts number into string
+// console.log(balance.toFixed(2)) //it gives descimal place upto two pr precision value
+// console.log(balance.toFixed(1))
+
+// const numb = 23.8966
+// console.log(numb.toFixed(2));
+// console.log(numb.toPrecision(3));
+
+// const numb1 = 123.8966
+// console.log(numb1.toPrecision(3));
+
+// const numb2 = 1123.8966
+// console.log(numb2.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+/******maths*****/
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // it converts  negative value to positive 
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.2)); // if it in decimal than this method round off in upper bound
+// console.log(Math.floor(4.9));// if it in decimal than this method round off in lower bound
+// console.log(Math.max(4,3,5,8,6));
+// console.log(Math.min(4,3,5,8,6));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min +1))+min);
+ 
