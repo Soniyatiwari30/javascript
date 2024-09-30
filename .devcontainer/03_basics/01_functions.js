e26c8b3bac4f4c45c3a7b556cf223f4b7eb65e33
Soniyatiwari30 <128225944+Soniyatiwari30@users.noreply.github.com>
2024-09-30 09:29:27 +0000

    functions in array and in objects

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -28,7 +28,7 @@ function addTwoNum(number1,number2){
 //  console.log("result:", result);
 
 
-function loginUserMessage(username = "sam"){  //in this we pass default vaulue so that when doest pass argument that it take it from here
+function loginUserMessage(username = "sam"){       //in this we pass default vaulue so that when doest pass argument that it take it from here
     if(!username){
         console.log("please enter user name");
         return
@@ -37,9 +37,44 @@ function loginUserMessage(username = "sam"){  //in this we pass default vaulue s
 }
 
 //console.log(loginUserMessage("soniya"));
-console.log(loginUserMessage());        //when u dont pass argument it gives undefined
+// console.log(loginUserMessage());           //when u dont pass argument it gives undefined
 
 
 
+function claculateCartPrice(...num1){      //...rest operator it tales many arguments in arrray form 
+return num1
+}
+// console.log(claculateCartPrice(200, 400, 500, 2000));
 
- 
+
+
+function claculateCartPrice1(val1,val2,...num1){
+    return num1
+    }
+//  console.log(claculateCartPrice1(200, 400, 500, 2000));
+
+
+
+const user = {
+    username1: "soni",
+    price: 100
+}
+
+function handleObject(anyobject) {
+console.log(`username is ${anyobject.username1} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username1:"sam",
+    price:1000
+})
+
+const myArr = [200, 400, 100, 500]
+
+function returnSecondValue(getArray){
+ return getArray[1]
+}
+
+// console.log(returnSecondValue(myArr));
+console.log(returnSecondValue([200, 400, 100, 500]));
