8c9a4c602812635ef400100de143458f14cb0114
Soniyatiwari30 <128225944+Soniyatiwari30@users.noreply.github.com>
2024-10-07 13:31:03 +0000

    for in

---
+++ .devcontainer/05_iterations/four.js
@@ -0,0 +1,27 @@
+//for in - used for objects
+
+const myObject ={
+    js : 'javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+    swift : "swit by app"    
+}
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+//for in - used in array
+const programming = ["js", "rb", "py", "java","cpp"]
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN','India')
+// map.set('USA','United States Of America')
+// map.set('FR','France')
+
+// for (const key in map) {
+//     console.log(key);   
+// }
